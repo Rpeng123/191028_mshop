3adc51634da578537556e30bf7ea6fdbdf6e8a52
Rpeng123 <1365926294@qq.com>
2019-10-28 16:26:31 +0800

    init mshop

---
+++ src/store/state.js
@@ -0,0 +1,14 @@
+/*
+状态对象
+ */
+export default {
+  latitude:23.638214,//纬度
+  longitude:113.68623,//经度
+  address:{},//地址相关信息对象
+  categorys: [],//食品分类数组
+  shops:[],    //商家数组
+  userInfo:{}, //用户信息
+  goods:[],//商品列表
+  ratings:[],//商家评价列表
+  info:[],//商家信息
+}
